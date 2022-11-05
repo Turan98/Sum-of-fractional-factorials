87f2eac6012d9bd4d44f1fe04a2ddec779a398ee
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-11-05 17:20:12 +0400

    test13

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double a ;
+double sum =0;
+double fuct = 1;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Enter a: " );
+a = Convert.ToDouble(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+     fuct = fuct * i;
+     sum += 1/fuct;
+    Console.WriteLine(fuct);
+  
+
+}
+Console.WriteLine(sum +1 );
